d40b5878c43e405abb450a234c6c3d755580b418
kro12 <croninomahoney@Cronins-iMac-2.local>
2014-01-28 23:34:43 +0000

    New Rails app with basic config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbone::Application.config.secret_token = 'bd89290159490165a6ea61f154eea772959ae2b42d7abf73506e37cade7e607f8a2cd024302a920478f687618c05fbcca2d7d5e6f087ab919bde4a46ff4b3f68'
